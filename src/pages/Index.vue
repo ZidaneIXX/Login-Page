3c3d16c7ad46a6fb2f62b58a2cd0c08a3a06ed05
Pedro <pedro.alves.2014@outlook.com>
2020-07-04 00:02:18 -0300

    First commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+     <q-card flat bordered class="my-card text-center">
+      <q-card-section>
+       <q-input outlined v-model="text" label="Email or phone number" />
+      <q-input outlined v-model="text" label="Password" class="q-mt-sm" />
+       <q-btn color="blue-10" label="Login" class="q-mt-sm full-width" no-caps />
+       <a href="#" style="font-size:10px;text-decoration:none">Forgot account?</a>
+      </q-card-section>
+
+      <q-separator inset />
+
+      <q-card-section>
+<q-btn color="green-7" label="Create New Account" style="padding:0" no-caps></q-btn>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style>
+</style>
